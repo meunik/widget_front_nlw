ca96daf2e3ace2fa298d469557231b9fa8c118c0
Marcos <meunik@live.com>
2022-06-09 17:53:56 -0300

    Primeiro

---
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -0,0 +1,27 @@
+import html2canvas from "html2canvas";
+import { Camera } from "phosphor-react";
+import { useState } from "react";
+
+export function ScreenshotButton() {
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
+
+    async function handleTakeScreenshot() {
+        setIsTakingScreenshot(true)
+
+        const canvas = await html2canvas(document.querySelector('html')!)
+        const base64image = canvas.toDataURL('image/png')
+
+        console.log(base64image);
+        setIsTakingScreenshot(false)
+    }
+
+    return (
+        <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            onClick={handleTakeScreenshot}
+        >
+            <Camera className="w-6 h-6"/>
+        </button>
+    )
+}
---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,60 @@
+import { CloseButton } from "../CloseButton";
+import { useState } from "react";
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedBackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagen de um inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagen de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: 'Outros',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagen de um balão de pensamento'
+        }
+    },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
+            )}
+            
+            <footer className="text-xl text-neutral-400">
+                Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+            </footer>
+        </div>
+    );
+}
