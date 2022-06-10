a1f01ca6f884b8bae252a8794980abb6fa0b9975
Marcos <meunik@live.com>
2022-06-10 12:04:23 -0300

    Front(sem integração com o back) pronto

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,8 +1,14 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
+import { Loading } from "./Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string|null;
+    onScreenshotTook: (screenshot: string|null) => void;
+}
+
+export function ScreenshotButton({screenshot, onScreenshotTook}: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
     async function handleTakeScreenshot() {
@@ -11,8 +17,25 @@ export function ScreenshotButton() {
         const canvas = await html2canvas(document.querySelector('html')!)
         const base64image = canvas.toDataURL('image/png')
 
-        console.log(base64image);
+        onScreenshotTook(base64image)
         setIsTakingScreenshot(false)
+    }
+
+    if (screenshot) {
+        return (
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => onScreenshotTook(null)}
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    // backgroundPosition: 'right bottom',
+                    // backgroundSize: 180
+                }}
+            >
+                <Trash weight="fill"/>
+            </button>
+        )
     }
 
     return (
@@ -21,7 +44,7 @@ export function ScreenshotButton() {
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
             onClick={handleTakeScreenshot}
         >
-            <Camera className="w-6 h-6"/>
+            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6"/> }
         </button>
     )
 }
